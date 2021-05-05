06f7dbdf7a8405c8c04699bcdff3d89550d756ba
张洋洋 <>
2021-05-05 17:40:09 +0800

    提交测试类

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.demo;
 
+import com.example.demo.until.CopyFile;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.File;
+
 @SpringBootTest
 class DemoApplicationTests {
+    private static int blockCount = 8;
 
     @Test
     void contextLoads() {
+        String srcPath = System.getProperty("user.dir")+"/src/main/resources/static/day14installpkg.zip";
+        String destPath = System.getProperty("user.dir")+"/src/main/resources/templates/day14installpkg.zip";
+        File soure = new File(srcPath);
+        long len = soure.length();
+        long oneNum = len/blockCount;
+        System.out.println(len);
+        System.out.println(oneNum);
+        for (int i = 0;i<blockCount-1;i++){
+            new CopyFile(srcPath,destPath,oneNum*i,oneNum*(i+1)).start();
+        }
+        //文件长度不能被整除的部分放在最后一段处理；
+        new CopyFile(srcPath,destPath,oneNum*(blockCount-1),len).start();
+
     }
 
 }
